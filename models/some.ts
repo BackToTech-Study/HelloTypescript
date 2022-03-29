ea2367eeb4b24a55c0030c9facb437efcc9d2594
VBota1 <viorel.bota@agilenetworks.tech>
2022-03-29 12:08:58 +0300

    added serialization examples

--- models/some.ts
+++ models/some.ts
@@ -3,7 +3,7 @@ import { ISome } from "./some_interface";
 export class Some implements ISome {
     public id: number = Math.floor(Math.random() * 999);
     public name: string = "";
-    public type: string = typeof(this);
+    private type: string = typeof(this);
 
     public sayHello() {
         return `Hello from ${this.name}.`
--- models/some_interface.ts
+++ models/some_interface.ts
@@ -1,4 +1,6 @@
 export interface ISome {
     id: number;
     name: string;
+
+    sayHello(): string;
 }
