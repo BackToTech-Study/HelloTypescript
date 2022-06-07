14317553e9067fab836406f6783d4a90c3351cc3
V Bota <VBota1@users.noreply.github.com>
2022-06-07 20:33:36 +0300

    added examples of asociative arrays

--- enum.ts
+++ enum.ts
@@ -15,3 +15,18 @@ console.log(`Color.BLUE.toString() = ${Color.BLUE.toString()}`)
 console.log(`Color[2] = ${Color[2]}`)
 console.log(`Color[Color.BLUE] = ${Color[Color.BLUE]}`)
 console.log(`Object.keys(Color) = ${Object.keys(Color)}`)
+
+
+var arr = new Array() 
+arr['Skoda'] = 0
+arr['BMW'] = 1
+arr['Dacia'] = 2
+arr['Volvo'] = 3
+
+console.log(`arr['Dacia'] = ${arr['Dacia']}`)
+console.log(`arr[2] = ${arr[2]}`)
+
+tmp = arr['Dacia']
+console.log(`tmp = ${tmp}`)
+
+console.log(`Masini['Dacia'] == arr['Dacia'] <=> ${Masini['Dacia'] == arr['Dacia']}`)
